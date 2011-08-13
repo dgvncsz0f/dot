9dba4632e849c8bb3da82441a35eef06508235d6
dsouza <dsouza@bitforest.org>
2011-08-13 17:33:42 -0300

    append-line/prepend-line functions

--- dot.libemacs/lib/myemacs.el
+++ dot.libemacs/lib/myemacs.el
@@ -1,6 +1,5 @@
-(defun my-open-shell-sideways 
-  () 
-  "Open shell in a window next to the current buffer"
+(defun my-open-shell-sideways ()
+  " Open shell in a window next to the current buffer "
   (interactive)
   (split-window-horizontally)
   (other-window 1) (shell))
@@ -18,5 +17,22 @@
   (let ((buffer-read-only t)
         (kill-read-only-ok t))
     (kill-line arg)))
+
+(defun my-prepend-line (&optional arg)
+  " Insert a newline before the current one. C-a C-o achieves the
+    same result.
+  "
+  (interactive "P")
+  (move-beginning-of-line arg)
+  (open-line 1))
+
+(defun my-append-line (&optional arg)
+  " Insert a newline after the current one. C-e C-o achieves the
+    same result.
+  "
+  (interactive "P")
+  (move-end-of-line arg)
+  (open-line 1)
+  (forward-line 1))
 
 (provide 'myemacs)
