0b4dc584a0381726cd59aeec2401337250fa39df
dsouza <dsouza@bitforest.org>
2011-08-13 10:14:33 -0300

    my-copy-line

--- dot.libemacs/lib/myemacs.el
+++ dot.libemacs/lib/myemacs.el
@@ -5,4 +5,18 @@
   (split-window-horizontally)
   (other-window 1) (shell))
 
+(defun my-copy-line (&optional arg)
+  " Do a kill-line but copy rather than kill.  This function
+    directly calls kill-line, so see documentation of kill-line
+    for how to use it including prefix argument and relevant
+    variables.  This function works by temporarily making the
+    buffer read-only.
+
+    Reference: http://www.emacswiki.org/emacs/CopyingWholeLines
+  "
+  (interactive "P")
+  (let ((buffer-read-only t)
+        (kill-read-only-ok t))
+    (kill-line arg)))
+
 (provide 'myemacs)
