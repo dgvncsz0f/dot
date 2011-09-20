a2df4d7134c0cae523b27c74816145bee604735b
dsouza <dsouza@bitforest.org>
2011-09-20 01:02:46 -0300

    configuring yasnippet

--- dot.libemacs/conf/yas.el
+++ dot.libemacs/conf/yas.el
@@ -1,3 +1,8 @@
 (require 'yasnippet-bundle)
 
 (yas/load-directory "~/.libemacs/yasnippets")
+
+(setq yas/dont-activate t)
+(global-set-key (kbd "C-c <C-tab>") 'yas/insert-snippet)
+(define-key yas/keymap (kbd "M-n") 'yas/next-field)
+(define-key yas/keymap (kbd "M-p") 'yas/prev-field)
