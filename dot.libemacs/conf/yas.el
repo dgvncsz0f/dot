9760512416c38b6810a63aa6ccd470f8df352c20
dsouza <dsouza@bitforest.org>
2011-09-23 22:52:01 -0300

    using the right function to expand templates

--- dot.libemacs/conf/yas.el
+++ dot.libemacs/conf/yas.el
@@ -3,6 +3,6 @@
 (yas/load-directory "~/.libemacs/yasnippets")
 
 (setq yas/dont-activate t)
-(global-set-key (kbd "C-c <C-tab>") 'yas/insert-snippet)
+(global-set-key (kbd "C-c <C-tab>") 'yas/expand)
 (define-key yas/keymap (kbd "M-n") 'yas/next-field)
 (define-key yas/keymap (kbd "M-p") 'yas/prev-field)
