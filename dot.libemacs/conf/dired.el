27af21df4edcdef4fda509122a96e4c0b5de6be4
dsouza <dsouza@bitforest.org>
2012-05-03 15:49:29 -0300

    defining it outside hook

--- dot.libemacs/conf/dired.el
+++ dot.libemacs/conf/dired.el
@@ -1,3 +1,3 @@
 (require 'dired)
 
-(add-hook 'dired-mode-hook (lambda () (when (not (member ".hi" dired-omit-extensions)) (setq dired-omit-extensions (cons ".hi" dired-omit-extensions)))))
+(when (not (member ".hi" dired-omit-extensions)) (setq dired-omit-extensions (cons ".hi" dired-omit-extensions)))
