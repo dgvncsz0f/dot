b235abd0e33201e7c5cf30b6b0ff24328d7898d8
dsouza <dsouza@bitforest.org>
2012-05-24 02:17:33 -0300

    named layouts // ewmh desktops

--- dot.xmonad/xmonad.hs
+++ dot.xmonad/xmonad.hs
@@ -1,27 +1,28 @@
 import XMonad
 import XMonad.Layout.Tabbed
+import XMonad.Layout.Named
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageDocks
 import XMonad.Actions.CopyWindow
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.SetWMName
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 import System.IO
 
-myConfig = return $ defaultConfig { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                                  , layoutHook = avoidStruts $ (simpleTabbed ||| tall)
-                                  , logHook    = do { dynamicLogWithPP dzenPP
-                                                    ; setWMName "LG3D"
-                                                    }
-                                  , modMask    = myModMask
-                                  , terminal   = "/usr/bin/urxvt -e /usr/bin/tmux"
-                                  , workspaces = myWorkspaces
+myConfig = return $ defaultConfig { manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
+                                  , layoutHook  = avoidStruts $ (lTabs ||| lTall ||| lFull)
+                                  , logHook     = dynamicLogWithPP dzenPP >> setWMName "LG3D"
+                                  , startupHook = setWMName "LG3D"
+                                  , modMask     = myModMask
+                                  , terminal    = "/usr/bin/urxvt -e /usr/bin/tmux"
+                                  , workspaces  = myWorkspaces
                                   } `additionalKeys` [ ((myModMask .|. shiftMask, xK_x),     spawn "/usr/bin/slock")
                                                      , ((myModMask .|. shiftMask, xK_s),     spawn "/usr/bin/import /tmp/screenshot.png")
                                                      , ((myModMask .|. shiftMask, xK_e),     spawn "/usr/bin/editor")
                                                      , ((myModMask .|. shiftMask, xK_b),     spawn "/usr/bin/x-www-browser")
-                                                     , ((myModMask .|. shiftMask, xK_space), spawn "/usr/bin/kupfer")
+                                                     , ((myModMask .|. shiftMask, xK_p),     spawn "/usr/bin/kupfer")
                                                      , ((myModMask .|. shiftMask, xK_equal), spawn "/usr/bin/aumix -w +10")
                                                      , ((myModMask .|. shiftMask, xK_minus), spawn "/usr/bin/aumix -w -10")
                                                      , ((myModMask,               xK_v),     windows copyToAll)
@@ -36,7 +37,9 @@ myConfig = return $ defaultConfig { manageHook = manageDocks <+> myManageHook <+
 
         myWorkspaces = map show [1..9]
 
-        tall = Tall 1 (3/100) (1/2)
+        lTall = named "tall" $ Tall 1 (3/100) (1/2)
+        lTabs = named "tabbed" simpleTabbed
+        lFull = named "full" Full
 
 main :: IO ()
-main = myConfig >>= xmonad
+main = fmap ewmh myConfig >>= xmonad
