a0037ce5cbb296911e4d68d619988741680919ec
diego souza <dsouza@c0d3.xxx>
2012-07-28 21:19:55 -0300

    no longer binding f8 to epresent run

--- dot.libemacs/conf/epresent.el
+++ dot.libemacs/conf/epresent.el
@@ -3,4 +3,3 @@
 (define-key epresent-mode-map (kbd "+") 'epresent-increase-font)
 (define-key epresent-mode-map (kbd "-") 'epresent-descrease-font)
 (define-key epresent-mode-map (kbd "l") 'linum-mode)
-(define-key org-mode-map (kbd "<f8>") 'epresent-run)
