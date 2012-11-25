fa8169177de5e3db32bc246af4b4f9c64fbb4ffa
dgvncsz0f <dsouza@c0d3.xxx>
2012-11-24 22:29:36 -0200

    moving gnupg stuff into its own module

---
+++ bash/xtra/gnupg/data/gnupg.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# -*- mode: Shell-script; sh-basic-offset: 2; -*-
+
+. "$HOME/.bash.d/functions"
+
+export GNUPGHOME="$HOME/sec/gnupg"
+
+my_test_ssh_connection ()
+{
+  test -n "$SSH_CONNECTION"
+}
+
+my_gpgagent_export()
+{
+  if [ -f "$HOME/.gpg-agent.env" ]
+  then
+    . "$HOME/.gpg-agent.env"
+    export GPG_AGENT_INFO
+    export SSH_AGENT_PID
+    export SSH_AUTH_SOCK
+    /usr/bin/gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+  fi
+}
+
+my_gpgagent_cleanse()
+{
+  my_gpgagent_export
+
+  if [ -x /usr/bin/gpg-connect-agent ]
+  then
+    if ! /usr/bin/gpg-connect-agent /bye 2>/dev/null
+    then
+      rm -f "$HOME/.gpg-agent.env"
+    fi
+  fi
+}
+
+my_gpgagent_init()
+{
+  local daemon_args
+
+  my_gpgagent_cleanse
+  if [ ! -f "$HOME/.gpg-agent.env" ]
+  then
+    if [ -d "$HOME/sec/gnupg" ]
+    then
+      /usr/bin/gpg-agent --daemon --sh --homedir "$HOME/sec/gnupg" --enable-ssh-support --write-env-file "$HOME/.gpg-agent.env" $daemon_args
+    fi
+  fi
+}
+
+my_test_interactive && ! my_test_ssh_connection && {
+  my_gpgagent_cleanse
+  my_gpgagent_init
+  my_gpgagent_export
+}
---
+++ bash/xtra/gnupg/dist/.bash.d/include.d/60gnupg.sh
@@ -0,0 +1,1 @@
+../../../data/gnupg.sh
