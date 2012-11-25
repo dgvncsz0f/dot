b07da6e926fc188c89db654292bb5433766bf4cc
dgvncsz0f <dsouza@c0d3.xxx>
2012-11-25 00:00:12 -0200

    no longer using data when there is only one file

--- bash/xtra/rvm/data/rvm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# -*- mode: Shell-script; sh-basic-offset: 2; -*-
-
-test -r "$HOME/.rvm/scripts/rvm" && . "$HOME/.rvm/scripts/rvm"
--- bash/xtra/rvm/dist/.bash.d/include.d/50rvm.sh
+++ bash/xtra/rvm/dist/.bash.d/include.d/50rvm.sh
@@ -1,1 +1,5 @@
-../../../data/rvm.sh
+#!/bin/bash
+# -*- mode: Shell-script; sh-basic-offset: 2; -*-
+
+export rvm_ignore_gemrc_issues=1
+test -r "$HOME/.rvm/scripts/rvm" && . "$HOME/.rvm/scripts/rvm"
