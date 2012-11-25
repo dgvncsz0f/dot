0cc8614c325d958bd9af014c0b8c58a2de97b221
dgvncsz0f <dsouza@c0d3.xxx>
2012-11-24 22:24:25 -0200

    refactoring encfs stuff into its own module

---
+++ bash/xtra/encfs/data/encfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- mode: Shell-script; sh-basic-offset: 2; -*-
+
+. $HOME/.bash.d/functions
+
+my_maybe_mount_encfs()
+{
+  local bin_encfs
+  local bin_mount
+  local bin_wc
+  local bin_grep
+
+  if which encfs 2>/dev/null >/dev/null
+  then
+    if [ -d "$HOME/.sec" ]
+    then
+      while ! mount | grep -q "$HOME/sec"
+      do
+        encfs "$HOME/.sec" "$HOME/sec" -o nonempty
+      done
+    fi
+  fi
+}
+
+my_test_interactive && my_maybe_mount_encfs
