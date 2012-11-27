09db85311d66904c96ff0b180696a09300581323
dgvncsz0f <dsouza@c0d3.xxx>
2012-11-27 18:10:18 -0200

    adding xinitrc

--- modules/terminal/shell/bash/xtra/rvm/dist/.bash.d/include.d/50rvm.sh
+++ modules/terminal/shell/bash/xtra/rvm/dist/.bash.d/include.d/50rvm.sh
@@ -2,4 +2,4 @@
 # -*- mode: Shell-script; sh-basic-offset: 2; -*-
 
 export rvm_ignore_gemrc_issues=1
-test -r "$HOME/.rvm/scripts/rvm" && . "$HOME/.rvm/scripts/rvm"
+my_test_interactive && test -r "$HOME/.rvm/scripts/rvm" && . "$HOME/.rvm/scripts/rvm"
