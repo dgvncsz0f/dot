394c788b33fd2477d529864df267ec564aa49fd1
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-03 12:32:32 -0200

    need to be authenticated before doing this

--- modules/dgvncsz0f/emacs/data/erc.el
+++ modules/dgvncsz0f/emacs/data/erc.el
@@ -39,8 +39,7 @@
 (setq erc-kill-server-buffer-on-quit t)
 (setq erc-autojoin-channels-alist
           '(("freenode" "#haskell" "#agda" "#latex" "#emacs" "##devel-pedreiro")
-            ("locaweb" "#cloud" "#infradev" "#linux" "#sysadms")
-            ("OFTC" "#sysadms")))
+            ("locaweb" "#cloud" "#infradev" "#linux" "#sysadms")))
 
 (defadvice erc-auto-query (around erc-auto-query-dirtyfix activate)
   (let ((old-erc-query-display erc-query-display))
