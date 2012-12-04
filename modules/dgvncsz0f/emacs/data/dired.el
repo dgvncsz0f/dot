1f5ed501046b59375e2ec1bc392772f8dd01520e
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-04 15:27:46 -0200

    M-n and M-p bound to next-error and previous error respectively

--- modules/dgvncsz0f/emacs/data/dired.el
+++ modules/dgvncsz0f/emacs/data/dired.el
@@ -1,3 +1,1 @@
-(add-hook 'dired-mode-hook (lambda () (define-key dired-mode-map (kbd "M-g") 'ack)
-                                      (define-key dired-mode-map (kbd "M-n") 'next-error)
-                                      (define-key dired-mode-map (kbd "M-p") 'previous-error)))
+(add-hook 'dired-mode-hook (lambda () (define-key dired-mode-map (kbd "M-g") 'ack)))
