c69cd6f39774cda4e2c25f9d4c7215a345c07e79
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-03 23:32:40 -0200

    removing stuff i dont use: trash & draft mostly

--- modules/dgvncsz0f/emacs/data/wanderlust-folders.el
+++ modules/dgvncsz0f/emacs/data/wanderlust-folders.el
@@ -9,12 +9,6 @@ locaweb {
 
 gmail {
   %inbox:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "inbox"
-  %[Gmail]/Trash:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "trash"
+  %ilists/haskell:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "haskell"
   %[Gmail]/Sent Mail:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "sent"
-  %[Gmail]/Drafts:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "drafts"
-  lists {
-    %cron:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "cron"
-    %ilists/haskell:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "l/haskell"
-    %ilists/netstack:"dsouza@bitforest.org"/clear@imap.gmail.com:993! "l/netstack"
-  }
 }
