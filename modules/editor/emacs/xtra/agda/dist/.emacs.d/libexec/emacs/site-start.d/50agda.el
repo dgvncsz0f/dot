c3a3192f826561e2cf35d7670fe6a5afa9ead57f
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-07 17:45:50 -0200

    agda-mode using standard faces

--- modules/editor/emacs/xtra/agda/dist/.emacs.d/libexec/emacs/site-start.d/50agda.el
+++ modules/editor/emacs/xtra/agda/dist/.emacs.d/libexec/emacs/site-start.d/50agda.el
@@ -1,2 +1,8 @@
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+
+(require 'agda2)
+(require 'agda2-highlight)
+
+(agda2-highlight-set-faces 'agda2-highlight-face-groups 'default-faces)
+(setq-default agda2-highlight-level 'interactive)
