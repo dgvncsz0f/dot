e3fd09c706e23fa4ee7fcac3650e9b9f891ec0cf
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-07 22:14:36 -0200

    ignoring errors if agda-mode is not installed

--- modules/editor/emacs/xtra/agda/dist/.emacs.d/libexec/emacs/site-start.d/50agda.el
+++ modules/editor/emacs/xtra/agda/dist/.emacs.d/libexec/emacs/site-start.d/50agda.el
@@ -1,8 +1,9 @@
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
+(ignore-errors
+  (load-file (let ((coding-system-for-read 'utf-8))
+               (shell-command-to-string "agda-mode locate 2>/dev/null"))))
 
-(require 'agda2)
-(require 'agda2-highlight)
-
-(agda2-highlight-set-faces 'agda2-highlight-face-groups 'default-faces)
-(setq-default agda2-highlight-level 'interactive)
+(when (featurep 'agda2)
+  (require 'agda2)
+  (require 'agda2-highlight)
+  (agda2-highlight-set-faces 'agda2-highlight-face-groups 'default-faces)
+  (setq-default agda2-highlight-level 'interactive))
