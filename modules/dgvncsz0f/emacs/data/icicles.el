42f3da1b038ca0249fa6d41c5d5d1ce6e4dfa503
dgvncsz0f <dsouza@c0d3.xxx>
2012-12-10 17:08:30 -0200

    changing default S-TAB completion method

--- modules/dgvncsz0f/emacs/data/icicles.el
+++ modules/dgvncsz0f/emacs/data/icicles.el
@@ -8,9 +8,10 @@
       (icicle-locate-file))))
 
 (defun my-icicle-mode-hook ()
-  (when (symbol-value icicle-mode)
+  (when icicle-mode
     (setq icicle-incremental-completion-flag t)
     (define-key icicle-mode-map (kbd "C-c f") 'my-locate-project-file)
-    (define-key icicle-mode-map (kbd "C-c ,") 'icicle-tag-search)))
+    (define-key icicle-mode-map (kbd "C-c ,") 'icicle-tag-search)
+    (setq icicle-apropos-complete-match-fn 'icicle-scatter-match)))
 
 (add-hook 'icicle-mode-hook 'my-icicle-mode-hook)
