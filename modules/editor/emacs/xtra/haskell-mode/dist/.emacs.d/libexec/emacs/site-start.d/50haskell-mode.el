9938d1c6d2d3252b7312b3d8559b053204073447
dgvncsz0f <dsouza@c0d3.xxx>
2013-07-23 15:01:19 -0300

    haskell-simple-indent

--- modules/editor/emacs/xtra/haskell-mode/dist/.emacs.d/libexec/emacs/site-start.d/50haskell-mode.el
+++ modules/editor/emacs/xtra/haskell-mode/dist/.emacs.d/libexec/emacs/site-start.d/50haskell-mode.el
@@ -1,3 +1,6 @@
 (unless (package-installed-p 'haskell-mode)
   (package-refresh-contents)
   (package-install 'haskell-mode))
+
+(custom-set-variables
+ '(haskell-mode-hook '(turn-on-haskell-simple-indent)))
