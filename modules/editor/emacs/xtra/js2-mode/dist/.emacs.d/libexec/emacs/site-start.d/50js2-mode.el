2f7da6cdc8e4bfc89e3dc9e2e0c13a902737684d
dgvncsz0f <dsouza@c0d3.xxx>
2014-12-09 20:09:40 -0200

    install js2-mode using package-install

--- modules/editor/emacs/xtra/js2-mode/dist/.emacs.d/libexec/emacs/site-start.d/50js2-mode.el
+++ modules/editor/emacs/xtra/js2-mode/dist/.emacs.d/libexec/emacs/site-start.d/50js2-mode.el
@@ -1,1 +1,7 @@
-../../../../../data/js2-mode.autoload
+; -*- mode: emacs-lisp; -*-
+
+(unless (package-installed-p 'js2-mode)
+  (package-install 'js2-mode))
+
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
