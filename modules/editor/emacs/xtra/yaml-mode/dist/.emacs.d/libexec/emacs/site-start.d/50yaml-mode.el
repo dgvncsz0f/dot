2eb81e42ca2f3319bf5ada5f7d989fbfdfce7e08
dgvncsz0f <dsouza@c0d3.xxx>
2014-12-10 19:21:57 -0200

    install yaml-mode using package-install

--- modules/editor/emacs/xtra/yaml-mode/dist/.emacs.d/libexec/emacs/site-start.d/50yaml-mode.el
+++ modules/editor/emacs/xtra/yaml-mode/dist/.emacs.d/libexec/emacs/site-start.d/50yaml-mode.el
@@ -1,1 +1,7 @@
-../../../../../data/yaml-mode.autoload
+; -*- mode: emacs-lisp; -*-
+
+(unless (package-installed-p 'yaml-mode)
+  (package-install 'yaml-mode))
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
