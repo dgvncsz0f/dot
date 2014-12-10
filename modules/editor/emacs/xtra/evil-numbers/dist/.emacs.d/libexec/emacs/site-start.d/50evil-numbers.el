e9eee3bcad55f88244407c87fbb6ffcffa59b611
dgvncsz0f <dsouza@c0d3.xxx>
2014-12-10 14:24:40 -0200

    install evil-numbers using package-install

--- modules/editor/emacs/xtra/evil-numbers/dist/.emacs.d/libexec/emacs/site-start.d/50evil-numbers.el
+++ modules/editor/emacs/xtra/evil-numbers/dist/.emacs.d/libexec/emacs/site-start.d/50evil-numbers.el
@@ -1,3 +1,8 @@
+; -*- mode: emacs-lisp; -*-
+
+(unless (package-installed-p 'evil-numbers)
+  (package-install 'evil-numbers))
+
 (require 'evil-numbers)
 
 (global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
