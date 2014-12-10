901579168cc8b5f1ecd7f08d6e457ad98007eda5
dgvncsz0f <dsouza@c0d3.xxx>
2014-12-10 19:20:13 -0200

    install yasnippet using package-install

--- modules/editor/emacs/xtra/yasnippet/dist/.emacs.d/libexec/emacs/site-start.d/50yasnippet.el
+++ modules/editor/emacs/xtra/yasnippet/dist/.emacs.d/libexec/emacs/site-start.d/50yasnippet.el
@@ -1,1 +1,9 @@
-../../../../../data/yasnippet.autoload
+; -*- mode: emacs-lisp; -*-
+
+(unless (package-installed-p 'yasnippet)
+  (package-install 'yasnippet))
+
+(add-to-list 'load-path (concat user-emacs-directory "share/emacs/site-lisp/yasnippet"))
+
+(require 'yasnippet)
+(yas-global-mode 1)
