bcb44ebf47791f94e88a558b9b7229a8d2c76a3e
dgvncsz0f <dsouza@c0d3.xxx>
2015-04-22 17:28:00 -0300

    solarized working on both X and terminal

--- modules/editor/emacs/xtra/solarized-theme/dist/.emacs.d/libexec/emacs/site-start.d/50solarized-theme.el
+++ modules/editor/emacs/xtra/solarized-theme/dist/.emacs.d/libexec/emacs/site-start.d/50solarized-theme.el
@@ -1,2 +1,8 @@
 (unless (package-installed-p 'color-theme-solarized)
   (package-install 'color-theme-solarized))
+
+(defun use-solarized-bgmode (frame mode)
+  (set-frame-parameter frame 'background-mode mode)
+  (when (not (display-graphic-p frame))
+    (set-terminal-parameter (frame-terminal frame) 'background-mode mode))
+  (enable-theme 'solarized))
