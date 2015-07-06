d9622cc1ba17c7a16c40c339589718ad9eae8d5c
dgvncsz0f <dsouza@c0d3.xxx>
2015-07-06 15:04:42 -0300

    keep single js2-mode config

--- modules/dgvncsz0f/emacs/dist/.emacs.d/libexec/emacs/site-start.d/99dgvncsz0f-js-mode.el
+++
@@ -1,6 +0,0 @@
-(require 'js)
-
-(defun my-js-mode-hook ()
-  (setq js-indent-level 2))
-
-(add-hook 'js-mode-hook 'my-js-mode-hook)
--- modules/editor/emacs/xtra/js2-mode/dist/.emacs.d/libexec/emacs/site-start.d/50js2-mode.el
+++ modules/editor/emacs/xtra/js2-mode/dist/.emacs.d/libexec/emacs/site-start.d/50js2-mode.el
@@ -3,5 +3,7 @@
 (unless (package-installed-p 'js2-mode)
   (package-install 'js2-mode))
 
-(autoload 'js2-mode "js2-mode" nil t)
+(require 'js)
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js-mode-hook (lambda () (setq js-indent-level 2)))
