8195390503459a7f583cc4ee53ae4a734a670706
dgvncsz0f <dsouza@c0d3.xxx>
2015-11-23 16:07:33 -0200

    add audio buttons [raise_vol, lower_vol and mute]

--- modules/x11/xmonad/dist/.xmonad/xmonad.hs
+++ modules/x11/xmonad/dist/.xmonad/xmonad.hs
@@ -1,28 +1,33 @@
 -- -*- mode: haskell; -*-
 
 import XMonad
-import XMonad.Actions.UpdatePointer
+import System.IO
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Layout.Named
 import XMonad.Actions.Warp
 import XMonad.Layout.Tabbed
-import XMonad.Layout.Named
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.ManageDocks
-import XMonad.Actions.CopyWindow
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.SetWMName
-import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
-import System.IO
+import Graphics.X11.Xlib.Misc
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.StackTile
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.CopyWindow
+import XMonad.Hooks.ManageHelpers
+import XMonad.Actions.UpdatePointer
 
 myConfig = return $ defaultConfig { manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                                  , layoutHook  = avoidStruts $ (lTabs ||| lTall ||| lFull)
-                                  , logHook     = dynamicLogWithPP dzenPP >> setWMName "LG3D" >> updatePointer (Relative 0 0)
+                                  , layoutHook  = avoidStruts $ (lTabs ||| lTall ||| lFull ||| lStck)
+                                  , logHook     = dynamicLogWithPP dzenPP >> setWMName "LG3D"
                                   , startupHook = setWMName "LG3D"
                                   , modMask     = myModMask
                                   , terminal    = "sakura"
                                   , workspaces  = myWorkspaces
                                   } `additionalKeys` [ ((myModMask, xK_x), spawn "slock")
+                                                     , ((noModMask, stringToKeysym "XF86AudioMute"), spawn "amixer set Master toggle")
+                                                     , ((noModMask, stringToKeysym "XF86AudioRaiseVolume"), spawn "amixer set Master 5dB+")
+                                                     , ((noModMask, stringToKeysym "XF86AudioLowerVolume"), spawn "amixer set Master 5dB-")
                                                      , ((myModMask .|. shiftMask, xK_x), spawn "xscreensaver-command -exit")
                                                      , ((myModMask .|. shiftMask, xK_s), spawn "import /tmp/screenshot.png")
                                                      , ((myModMask .|. shiftMask, xK_e), spawn "bash -l -c editor")
@@ -43,6 +48,7 @@ myConfig = return $ defaultConfig { manageHook  = manageDocks <+> myManageHook <
         lTall = named "tall" $ Tall 1 (3/100) (1/2)
         lTabs = named "tabbed" simpleTabbed
         lFull = named "full" Full
+        lStck = named "stack" $ StackTile 1 (3/100) (1/2)
 
 main :: IO ()
 main = fixEncoding >> fmap ewmh myConfig >>= xmonad
