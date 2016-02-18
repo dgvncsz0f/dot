0f9809ce4159185f3d8c92f76317d7e385ba8896
dgvncsz0f <dsouza@c0d3.xxx>
2016-02-18 11:40:16 -0200

    smart scripts: xinitrc & xmodmaprc

--- modules/x11/xmonad/dist/.xmonad/xmonad.hs
+++ modules/x11/xmonad/dist/.xmonad/xmonad.hs
@@ -1,5 +1,3 @@
--- -*- mode: haskell; -*-
-
 import XMonad
 import System.IO
 import XMonad.Util.Run (spawnPipe)
@@ -17,23 +15,28 @@ import XMonad.Actions.CopyWindow
 import XMonad.Hooks.ManageHelpers
 import XMonad.Actions.UpdatePointer
 
-myConfig = return $ defaultConfig { manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                                  , layoutHook  = avoidStruts $ (lTabs ||| lTall ||| lFull ||| lStck)
-                                  , logHook     = dynamicLogWithPP dzenPP >> setWMName "LG3D"
-                                  , startupHook = setWMName "LG3D"
-                                  , modMask     = myModMask
-                                  , terminal    = "sakura"
-                                  , workspaces  = myWorkspaces
-                                  } `additionalKeys` [ ((myModMask, xK_x), spawn "slock")
-                                                     , ((noModMask, stringToKeysym "XF86AudioMute"), spawn "amixer set Master toggle")
-                                                     , ((noModMask, stringToKeysym "XF86AudioRaiseVolume"), spawn "amixer set Master 5dB+")
-                                                     , ((noModMask, stringToKeysym "XF86AudioLowerVolume"), spawn "amixer set Master 5dB-")
-                                                     , ((myModMask .|. shiftMask, xK_x), spawn "xscreensaver-command -exit")
-                                                     , ((myModMask .|. shiftMask, xK_s), spawn "import /tmp/screenshot.png")
-                                                     , ((myModMask .|. shiftMask, xK_e), spawn "bash -l -c editor")
-                                                     , ((myModMask .|. shiftMask, xK_b), spawn "bash -l -c conkeror")
-                                                     , ((myModMask              , xK_p), spawn "bash -l -c dmenu_run")
-                                                     , ((myModMask              , xK_z), warpToWindow 0 0)
+
+
+myConfig = do
+  xmproc <- spawnPipe "xmobar --bottom --dock"
+  return $ defaultConfig { manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
+                         , layoutHook  = avoidStruts (lTabs ||| lTall ||| lFull ||| lStck)
+                         , logHook     = dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn xmproc }
+
+                         , startupHook = setWMName "LG3D"
+                         , modMask     = myModMask
+                         , terminal    = "sakura"
+                         , workspaces  = myWorkspaces
+                         } `additionalKeys` [ ((myModMask, xK_x), spawn "slock")
+                                            , ((noModMask, stringToKeysym "XF86AudioMute"), spawn "amixer set Master toggle")
+                                            , ((noModMask, stringToKeysym "XF86AudioRaiseVolume"), spawn "amixer set Master 5dB+")
+                                            , ((noModMask, stringToKeysym "XF86AudioLowerVolume"), spawn "amixer set Master 5dB-")
+                                            , ((myModMask .|. shiftMask, xK_x), spawn "xscreensaver-command -exit")
+                                            , ((myModMask .|. shiftMask, xK_s), spawn "import /tmp/screenshot.png")
+                                            , ((myModMask .|. shiftMask, xK_e), spawn "bash -l -c editor")
+                                            , ((myModMask .|. shiftMask, xK_b), spawn "bash -l -c conkeror")
+                                            , ((myModMask              , xK_p), spawn "bash -l -c dmenu_run")
+                                            , ((myModMask              , xK_z), warpToWindow 0 0)
                                                      ]
   where myManageHook = composeAll [ className =? "Pidgin"      --> doFloat
                                   , className =? "Skype"       --> doFloat
