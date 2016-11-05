f820fd7c2ec40b7700d4347ecc3e227ca34ac9a5
dgvncsz0f <dsouza@c0d3.xxx>
2016-11-05 17:47:47 -0200

    sml respectful theme

--- modules/editor/emacs/xtra/smart-mode-line/dist/.emacs.d/libexec/emacs/site-start.d/99sml.el
+++ modules/editor/emacs/xtra/smart-mode-line/dist/.emacs.d/libexec/emacs/site-start.d/99sml.el
@@ -5,8 +5,8 @@
 
 (require 'smart-mode-line)
 
-(setq sml/theme 'dark)
 (setq rm-blacklist ".")
-(setq sml/no-confirm-load-theme t)
+(setq sml/theme 'respectful)
+(setq sml/no-confirm-load-them t)
 
 (sml/setup)
