94ad226b90a3320701388126efda7ad7b72f0b48
dgvncsz0f <dsouza@c0d3.xxx>
2017-05-02 14:51:46 -0300

    bugfix: autorandr.sh for work

--- bundles/dsouza@work/autorandr/dist/.bash.d/bin/autorandr.sh
+++ bundles/dsouza@work/autorandr/dist/.bash.d/bin/autorandr.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 hdmi1_enabled () {
-  xrandr -q | grep HDMI1 | grep -qv disconnected
+  xrandr -q | grep HDMI-1 | grep -qv disconnected
 }
 
 hdmi1_connect () {
-  xrandr --output eDP1 --auto --left-of HDMI1 --output HDMI1 --auto
+  xrandr --output eDP-1 --auto --left-of HDMI-1 --output HDMI-1 --auto
 }
 
 hdmi1_disconnect () {
-  xrandr --output eDP1 --auto --output HDMI1 --off
+  xrandr --output eDP-1 --auto --output HDMI-1 --off
 }
 
 hdmi1_state=-1
