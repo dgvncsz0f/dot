682c2010bb46f6a2b369b0bcb97524777610f62e
dgvncsz0f <dsouza@c0d3.xxx>
2017-07-25 23:23:17 -0300

    add intero package

--- modules/editor/emacs/xtra/haskell-mode/dist/.emacs.d/libexec/emacs/site-start.d/50haskell-mode.el
+++ modules/editor/emacs/xtra/haskell-mode/dist/.emacs.d/libexec/emacs/site-start.d/50haskell-mode.el
@@ -1,5 +1,9 @@
 (unless (package-installed-p 'haskell-mode)
   (package-install 'haskell-mode))
 
+(unless (package-installed-p 'intero)
+  (package-install 'intero))
+
 (custom-set-variables
  '(haskell-mode-hook '(turn-on-haskell-indentation)))
+(add-hook 'haskell-mode-hook 'intero-mode)
