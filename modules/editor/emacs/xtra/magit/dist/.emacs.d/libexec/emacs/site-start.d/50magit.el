b9ce5180ace8b564e7ea06aea8f80cc4f98eaea5
dgvncsz0f <dsouza@c0d3.xxx>
2017-08-18 11:19:38 -0300

    tune magit for performance

--- modules/editor/emacs/xtra/magit/dist/.emacs.d/libexec/emacs/site-start.d/50magit.el
+++ modules/editor/emacs/xtra/magit/dist/.emacs.d/libexec/emacs/site-start.d/50magit.el
@@ -1,2 +1,6 @@
 (unless (package-installed-p 'magit)
   (package-install 'magit))
+
+(setq vc-handled-backends nil)
+(remove-hook 'server-switch-hook 'magit-commit-diff)
+(remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
